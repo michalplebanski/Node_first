ec9aedfd2911e10cb77917ee4e24969722456c6a
Michal <michalplebanski@gmail.com>
2018-04-05 11:22:05 +0200

    first commit

---
+++ program.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if(input !== null) {
+		var instruction = input.toString().trim();
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app! \n');
+				process.exit();
+				break;
+			case '/version':
+				console.log(process.versions);
+				break;
+			case '/lang':
+				console.log(process.env.LANG);
+				break;
+			default:
+				process.stderr.write('Wrong instruction! \n');
+		}
+	}
+});
